8212ce13a51a1e7369d143eb3e570ac7b2532c52
shelby3000 <vamsisiri345@gmail.com>
2025-12-16 21:32:31 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
